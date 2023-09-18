b20fa6162c3cf2ebfaecbb808f5dea5ea96d202e
HasanOmar1 <r3domar1@gmail.com>
2023-09-18 19:05:15 +0300

    added

---
+++ 5.3password.js
@@ -0,0 +1,25 @@
+let password = prompt('Type your password:');
+
+
+// if (password.length > 7){
+//     console.log('Strong');
+// }
+
+// else{
+//     console.log('Weak');
+// }
+
+
+password.length > 7 ? console.log('Strong') : console.log('Weak');
+
+
+// if (password.length >= 0 && password.length < 8){
+//     console.log('Weak');
+// }
+// else{
+//     console.log('Strong');
+// }
+
+
+
+

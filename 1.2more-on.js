5e2497a48a650f468e92852025b1ddfe07a9131d
HasanOmar1 <r3domar1@gmail.com>
2023-09-18 12:03:16 +0300

    added

---
+++ 1.2more-on.js
@@ -0,0 +1,55 @@
+// 1. Which of the following is/are strings?
+// c) '4'
+
+// 2. Which of the following is/are numbers?
+// a) 4
+// b) 4.0
+// d) -4
+
+// 3. Which of the following is/are booleans?
+// a) true
+// b) false
+
+// 4. What is the result of 80 + 71.2?
+// a) 151.2
+
+// 5. What is the result of "80" + 71.2?
+// c) "8071.2"
+
+
+// 6. Does 100 + 30 produce a number or a string?
+// a) number
+
+
+// 7. Does "100" + 30 produce a number or a string?
+// b)string
+
+
+var one = 10;
+let two = 20;
+const three = 30;
+
+let kola = 91;
+kola += 9;
+console.log(kola);
+
+let asd;
+asd = "He's got it!";
+console.log(asd);
+
+let payBill = 99;
+let bill = `The bill is :$${payBill}`;
+console.log(bill);
+
+let payTax = 410;
+let tax = `You have to pay: $${payTax}`;
+console.log(tax);
+
+let fullPrice = payBill * payTax ;
+let total = `Your total bill is : $${fullPrice}`;
+console.log(total);
+
+console.log(Math.round(50.6));
+
+let sndl;
+console.log(sndl);

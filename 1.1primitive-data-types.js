a23e54c02efe2055450a7da16fc4da1206ffead8
HasanOmar1 <r3domar1@gmail.com>
2023-09-18 11:45:40 +0300

    added

---
+++ 1.1primitive-data-types.js
@@ -0,0 +1,19 @@
+
+
+let name = 'Hasan';
+let number = 2;
+let bool = true;
+let kola = null;
+let sup;
+let sym = Symbol('Hi');
+const car = {type:"Fiat", model:"500", color:"white"};
+
+
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof bool);
+console.log(typeof kola);
+console.log(typeof sup);
+console.log(typeof sym);
+console.log(typeof car);
+

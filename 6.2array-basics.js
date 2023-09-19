2f632a6722fc8df1d2f70604f11265fe2184c54d
HasanOmar1 <r3domar1@gmail.com>
2023-09-19 10:47:49 +0300

    added

--- 6.2array-basics.js
+++ 6.2array-basics.js
@@ -8,13 +8,15 @@ people.push('Hasan');
 const people2 = people.slice(2);
 
 // console.log(people.indexOf('Mary'));
-console.log(people.indexOf('Foo')); // it returns -1 because its not found in the array.
+// console.log(people.indexOf('Foo')); // it returns -1 because its not found in the array.
 
 
 
 people = ['Greg' , 'Mary' , 'Devon', 'James'];
 
+people.splice(2 , 1 , 'Elizabeth' , 'Artie');
 
-
+let withBob = people.concat(['Bob']);
 
 console.log(people);
+console.log(withBob);

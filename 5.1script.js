fc3ccc97c2ed6253d68f7db19eb36462664d2ee5
HasanOmar1 <r3domar1@gmail.com>
2023-09-19 10:41:58 +0300

    Added

--- 5.1script.js
+++ 5.1script.js
@@ -1,20 +1,36 @@
 
-let language = prompt('Language?');
-let isIsland = prompt('Island? [ true / false]');
-let population = parseInt(prompt('Population?')); //25 million
-let country = prompt('Country?');
+// let language = prompt('Language?');
+// let isIsland = prompt('Island? [ true / false]');
+// let population = parseInt(prompt('Population?')); //25 million
+// let country = prompt('Country?');
 
 
 
 
-if (language === 'English' && isIsland === 'false' && population < 50000000 && country){ 
+// if (language === 'English' && isIsland === 'false' && population < 50000000 && country){ 
 
-    console.log(` You should live in ${country}`);
+//     console.log(` You should live in ${country}`);
 
-}
+// }
 
-else if (!(language === 'English' && isIsland === 'false' && population < 50000000 && country)){ 
+// else if (!(language === 'English' && isIsland === 'false' && population < 50000000 && country)){ 
 
-    console.log(`${country} does not meet your criteria`);
+//     console.log(`${country} does not meet your criteria`);
 
+// }
+
+
+//Shaddy's answer :
+
+const language = prompt('language');
+let isIsland = prompt('isIsland');
+const population = parseInt(prompt('population'));
+const country = prompt('country');
+
+isIsland == 'false' ? isIsland = false : isIsland=true;
+
+if(language === 'english' && population < 50 && !isIsland) {
+    console.log(`You should live in ${country}`)
+} else {
+    console.log(`${country} does not meet yout criteria`)
 }
---
+++ 6.2array-basics.js
@@ -0,0 +1,20 @@
+let people = ['Greg' , 'Mary' , 'Devon', 'James'];
+
+people.shift();
+people.pop();
+people.unshift('Matt');
+people.push('Hasan');
+
+const people2 = people.slice(2);
+
+// console.log(people.indexOf('Mary'));
+console.log(people.indexOf('Foo')); // it returns -1 because its not found in the array.
+
+
+
+people = ['Greg' , 'Mary' , 'Devon', 'James'];
+
+
+
+
+console.log(people);

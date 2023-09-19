c6597adb551dcc30eaabbcddef8b137cb4a76211
HasanOmar1 <r3domar1@gmail.com>
2023-09-19 11:17:16 +0300

    added

--- 6.2array-basics.js
+++ 6.2array-basics.js
@@ -1,18 +1,22 @@
-let people = ['Greg' , 'Mary' , 'Devon', 'James'];
+const people = ['Greg' , 'Mary' , 'Devon', 'James'];
 
 people.shift();
 people.pop();
 people.unshift('Matt');
 people.push('Hasan');
 
-const people2 = people.slice(2);
+// const people2 = people.slice(2);
+// console.log(people2);
 
 // console.log(people.indexOf('Mary'));
 // console.log(people.indexOf('Foo')); // it returns -1 because its not found in the array.
 
 
 
-people = ['Greg' , 'Mary' , 'Devon', 'James'];
+people.shift();
+people.unshift('Greg');
+people.pop();
+people.push('James');
 
 people.splice(2 , 1 , 'Elizabeth' , 'Artie');
 
---
+++ 6.3length-of-words.js
@@ -0,0 +1,21 @@
+let array = [`He` , `Hel` , `Hell`, `Hello`, `SpongeBob`]
+
+// let two = array.length
+// let three = array.indexOf(`Hel`);
+// let four = array.indexOf(`Hell`);
+// let five = array.indexOf(`Hello`);
+// let nine = array.indexOf(`SpongeBob`);
+
+let he = array[0].length;
+console.log(he);
+
+// console.log(array.indexOf(`Hel`));
+
+
+
+// console.log(array);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+// console.log(five);
+// console.log(nine);

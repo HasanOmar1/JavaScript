324f7228fb54e608361cca57bb5741d11083dad8
HasanOmar1 <r3domar1@gmail.com>
2023-09-19 14:25:34 +0300

    added

---
+++ 9.1loops.js
@@ -0,0 +1,14 @@
+const listOfNeighbours = [
+    
+    ["Canada", "Mexico"],
+    ["Spain"],
+    ["Norway", "Sweden", "Russia"],
+
+    ];
+
+    console.log(`Neighbour : ` + listOfNeighbours[0][0]);
+    console.log(`Neighbour : ` + listOfNeighbours[0][1]);
+    console.log(`Neighbour : ` + listOfNeighbours[1][0]);
+    console.log(`Neighbour : ` + listOfNeighbours[2][0]);
+    console.log(`Neighbour : ` + listOfNeighbours[2][1]);
+    console.log(`Neighbour : ` + listOfNeighbours[2][2]);

35bdd3a811b39464d026737e05b6b51f8ece3b40
HasanOmar1 <r3domar1@gmail.com>
2023-09-20 17:40:28 +0300

    added

--- 8.4counts-letters.js
+++ 8.4counts-letters.js
@@ -1,49 +1,13 @@
 
-// function count(array){
 
-    
-// }
-
-// const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
-// // const array = 'aAAAAABBbbyyy'
-// let counts = {};
-
-// for (let i in array){
-//     const letter = array[i].toLowerCase();
-//     console.log(letter)
-//     if(letter === " ") continue;
-//     else if(counts[letter] == undefined){
-//         counts[letter] = 1;
-//     }
-//     else {
-//         counts[letter] = counts[letter] +1; 
-//         }
-// }
-
-// console.log(counts);
-
-// const array1 = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
-
-// function arr(array){
-
-//     array1.forEach((element) => console.log(element))
-        
-//     ;
-// }
-// for ( let i in array1){
-
-//     array1.forEach((index) => console.log(index));
+const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"] ;
 
 
+let splits = array.join('');
+    counts = {};
 
-// }
-const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+splits.split('').forEach(function (str) {
+    counts[str.toLowerCase()] = (counts[str.toLowerCase()] || 0) + 1;
+});
 
-
-
-function countChars(text) {
-    return text
-      .split('');
-  }
-  console.log(countChars('array'));
-  //["H", "e", "l", "l", "o"]
+console.log(counts);

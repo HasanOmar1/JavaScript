98c9c549ebe7b924773ddc0516c2ae18ea3b8d43
HasanOmar1 <r3domar1@gmail.com>
2023-09-20 20:04:28 +0300

    added

--- 4.4boom.js
+++ 4.4boom.js
@@ -1,57 +1,5 @@
 
 
-
-// function bomb(n){
-// const arr = [];
-
-// for(let i = 1 ; i <= n ; i++){
-//     // arr.push(i);
-//     if( i % 7 === 0 && i.toString.indexOf("7")>-1){
-//      console.log('BOOM-BOOM'); 
-//     }
-
-//      else if(i % 7 === 0) {
-//         console.log('BOOM');
-
-//     }
-    
-//     else {
-//         console.log(i);
-
-//     }
-// }
-// console.log(arr[i])
-
-// // return arr[i];
-// // console.log(arr)
-
-// }
-// for(let i = 1 ; i <= 70 ; i++){
-//     let arr = [];
-//     console.log(arr[i]);
-// }
-
-
-// const divideOrHasSeven = (n) => {
-//     let arr = [];
-//     for (i = 1; i <= n; i++) {
-//         arr.push(i);
-//       if (i % 7 === 0 && i.toString().indexOf("7")> -1) { 
-//         arr.push(`BOOM-BOOM`)
-//       }
-//       else if(i % 7 === 0){
-//         arr.push('Boom')     }
-
-// }
-
-// return arr;
-
-//   };
-
-//   console.log(divideOrHasSeven(70));
-
-
-
 const divideOrHasSeven = (n) => {
     let arr = [];
     for (i = 1; i <= n; i++) {

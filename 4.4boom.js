6bb47be50d622ddc607661b0eac55b01cf8d2892
HasanOmar1 <r3domar1@gmail.com>
2023-09-20 20:03:35 +0300

    added

--- 4.4boom.js
+++ 4.4boom.js
@@ -1,41 +1,74 @@
 
 
-// const numbers = [];
-// let n = 18;
 
-// for(let i = 0 ; i<=n ; i++){
-//     numbers.push(i);
+// function bomb(n){
+// const arr = [];
 
-// if(i % 7 === 0 ){
-//     console.log(`BOOM`)
-// }
-// }
+// for(let i = 1 ; i <= n ; i++){
+//     // arr.push(i);
+//     if( i % 7 === 0 && i.toString.indexOf("7")>-1){
+//      console.log('BOOM-BOOM'); 
+//     }
 
-// let n = 12;
-// let arr = boom(n);
-// console.log(numbers)
+//      else if(i % 7 === 0) {
+//         console.log('BOOM');
 
-// function num(i){
-
-const arr = [];
-
-for(let i = 1 ; i <= 70 ; i++){
-    arr.push(i);
-    if( i % 7 === 0 && i === 7){
-     console.log('BOOM-BOOM'); 
-    }
-
-     else if(i % 7 === 0) {
-        console.log('BOOM');
-
-    }
+//     }
     
-    else {
-        // arr.push(i)
-        console.log(i);
+//     else {
+//         console.log(i);
 
-    }
+//     }
+// }
+// console.log(arr[i])
+
+// // return arr[i];
+// // console.log(arr)
+
+// }
+// for(let i = 1 ; i <= 70 ; i++){
+//     let arr = [];
+//     console.log(arr[i]);
+// }
+
+
+// const divideOrHasSeven = (n) => {
+//     let arr = [];
+//     for (i = 1; i <= n; i++) {
+//         arr.push(i);
+//       if (i % 7 === 0 && i.toString().indexOf("7")> -1) { 
+//         arr.push(`BOOM-BOOM`)
+//       }
+//       else if(i % 7 === 0){
+//         arr.push('Boom')     }
+
+// }
+
+// return arr;
+
+//   };
+
+//   console.log(divideOrHasSeven(70));
+
+
+
+const divideOrHasSeven = (n) => {
+    let arr = [];
+    for (i = 1; i <= n; i++) {
+      if (i % 7 === 0 && i.toString().indexOf("7")> -1) { 
+        arr.push(`BOOM-BOOM`)
+      }
+      else if(i % 7 === 0){
+        arr.push('Boom')     }
+        else{
+            arr.push(i);
+        }
+
 }
 
-// }
+return arr;
+
+  };
+
+  console.log(divideOrHasSeven(70));
 

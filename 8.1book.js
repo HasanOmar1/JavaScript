8453d035e6e17850f8d07578820bb8986d0edab7
HasanOmar1 <r3domar1@gmail.com>
2023-09-20 10:40:30 +0300

    added

---
+++ 8.1book.js
@@ -0,0 +1,9 @@
+const book = {
+    bookName : 'Harry Potter and the Cursed Child',
+    author : 'J.K. Rowling',
+    pages : '336' ,
+    genre : 'Fantasy',
+    year : '2016'
+};
+
+console.log(`The book ${book.bookName} was written by ${book.author} in the year ${book.year}.`)

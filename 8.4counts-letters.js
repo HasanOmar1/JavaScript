212bfde46ea7ec09cfbd4412389cea7158c5ca87
HasanOmar1 <r3domar1@gmail.com>
2023-09-20 17:43:44 +0300

    added

--- 8.4counts-letters.js
+++ 8.4counts-letters.js
@@ -6,8 +6,8 @@ const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"] ;
 let splits = array.join('');
     counts = {};
 
-splits.split('').forEach(function (str) {
-    counts[str.toLowerCase()] = (counts[str.toLowerCase()] || 0) + 1;
+splits.split('').forEach(function (array) {
+    counts[array.toLowerCase()] = (counts[array.toLowerCase()] || 0) + 1;
 });
 
 console.log(counts);

ffe5c979ad144612e541782c2b794c29c5f23599
HasanOmar1 <r3domar1@gmail.com>
2023-09-21 10:00:42 +0300

    added

---
+++ 2.1functions.js
@@ -0,0 +1,19 @@
+function countryInfo(country , population , capitalCity){
+
+
+return `${country} has ${population} people and its capital is ${capitalCity}`
+
+
+}
+
+console.log(countryInfo('Spain' , '47 Milion' , 'Madrid'))
+
+
+const USA = countryInfo('USA' , '332 Milion' , 'Washington D.C');
+const germany = countryInfo('Germany' , '83 Milion' , 'Berlin')
+const france = countryInfo('France' , '67 Milion' , 'Paris')
+
+console.log(USA);
+console.log(germany);
+console.log(france);
+

e4403716c17dbba1bbc08ad58ad6622f7319c644
HasanOmar1 <r3domar1@gmail.com>
2023-09-26 11:52:44 +0300

    added

---
+++ 12.1library.js
@@ -0,0 +1,29 @@
+function canBeRead(){
+
+        const library = [
+            {
+            author: "Bill Gates",
+            title: "The Road Ahead",
+            readingStatus: true
+            },
+            {
+            author: "Steve Jobs",
+            title: "Walter Isaacson",
+            readingStatus: true
+            },
+            {
+            author: "Suzanne Collins",
+            title: "Mockingjay: The Final Book of The Hunger Games",
+            readingStatus: false
+            }
+            ];
+
+            let readAble = library.filter(status => {
+            return status.readingStatus === true;
+        
+        })
+        return readAble;
+    }
+
+    console.log(canBeRead())
+   
